8aac7b2fcdcee4ea97bf9c4fe81912f09d5796ce
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-03-04 22:49:48 +0100

    update app component and others configs

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,49 @@
-import './App.scss';
+import "./App.css";
+import * as React from "react";
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme
+} from "@mantine/core";
+import { useHotkeys, useLocalStorage } from "@mantine/hooks";
+import { HeaderResponsive } from "../HeaderResponsive/HeaderResponsive";
+import { Subgrid } from "../Subgrid/Subgrid";
+import FooterSocial from "../FooterSocial/FooterSocial";
+import AffixButton from "../AffixButton/AffixButton";
 
-function App() {
+interface IAppProps {}
+
+const App: React.FC<IAppProps> = props => {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+    getInitialValueInEffect: true
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  useHotkeys([["mod+J", () => toggleColorScheme()]]);
+
   return (
-    <div className="App">
-
-    </div>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <div className="App">
+          <HeaderResponsive />
+          <Subgrid />
+          <FooterSocial />
+          <AffixButton />
+        </div>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
-}
+};
 
 export default App;
