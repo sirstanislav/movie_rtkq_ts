a1f572e1e02ad26a997717435dbb9e7ea9591322
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-03-04 22:47:00 +0100

    add footer component

---
+++ src/components/FooterSocial/FooterSocial.tsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+import {
+  createStyles,
+  Container,
+  Group,
+  ActionIcon,
+  rem,
+  Image,
+  Button
+} from "@mantine/core";
+import {
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandInstagram
+} from "@tabler/icons-react";
+import { MantineLogo } from "@mantine/ds";
+import logo from "../../images/logo.png";
+
+const useStyles = createStyles(theme => ({
+  footer: {
+    marginTop: rem(120),
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
+    }`
+  },
+
+  inner: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+
+    [theme.fn.smallerThan("xs")]: {
+      flexDirection: "row"
+    }
+  },
+
+  links: {
+    [theme.fn.smallerThan("xs")]: {
+      marginTop: theme.spacing.md
+    }
+  }
+}));
+
+interface IFooterSocialProps {}
+
+const FooterSocial: React.FC<IFooterSocialProps> = props => {
+  const { classes } = useStyles();
+
+  return (
+    <div className={classes.footer}>
+      <Container className={classes.inner}>
+        <Image maw={40} radius="md" src={logo} alt="Random image" />
+        <Group spacing={0} className={classes.links} position="right" noWrap>
+          <ActionIcon
+            size="lg"
+            component="a"
+            href="https://twitter.com/ctacbarada"
+            target="_blank"
+          >
+            <IconBrandTwitter size="1.05rem" stroke={2} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </div>
+  );
+};
+
+export default FooterSocial;
