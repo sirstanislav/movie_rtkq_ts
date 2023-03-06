e255cc70b2d4e29834daac1f53c020cadba0c9db
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-03-06 23:27:13 +0100

    fix: position and styles

--- src/components/AffixButton/AffixButton.tsx
+++ src/components/AffixButton/AffixButton.tsx
@@ -2,15 +2,32 @@ import * as React from "react";
 import { IconArrowUp } from "@tabler/icons-react";
 import { useWindowScroll } from "@mantine/hooks";
 import { Affix, Button, Text, Transition, rem } from "@mantine/core";
+import { useAppSelector } from "../../redux/hooks";
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles(theme => ({
+  root: {
+    [theme.fn.smallerThan("sm")]: {
+      left: 20
+    }
+  }
+}));
 
 interface IAffixButtonProps {}
 
 const AffixButton: React.FunctionComponent<IAffixButtonProps> = props => {
   const [scroll, scrollTo] = useWindowScroll();
+  const { isOpen } = useAppSelector(state => state.tweetData);
+  const { classes } = useStyles();
 
   return (
     <>
-      <Affix position={{ bottom: rem(100), right: rem(20) }}>
+      <Affix
+        zIndex={0}
+        position={{ bottom: rem(100), right: rem(20) }}
+        display={isOpen ? "none" : "block"}
+        className={classes.root}
+      >
         <Transition transition="slide-up" mounted={scroll.y > 0}>
           {transitionStyles => (
             <Button
