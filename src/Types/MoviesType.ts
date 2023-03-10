58cb5480ed249f9d5ba4942dd0019f37df00cb11
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-03-10 23:03:02 +0100

    add: types for movie result

---
+++ src/Types/MoviesType.ts
@@ -0,0 +1,24 @@
+export interface Result {
+  poster_path: string;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  genre_ids: number[];
+  id: number;
+  original_title: string;
+  original_language: string;
+  title: string;
+  backdrop_path: string;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  vote_average: number;
+  like?: boolean;
+}
+
+export interface MoviesArray {
+  page: number;
+  results: Result[];
+  total_results: number;
+  total_pages: number;
+}
